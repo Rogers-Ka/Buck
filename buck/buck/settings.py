8afb87cf48b33dc0bb17280fcbfee6e89cd1af5c
Rogers-Ka <76101490+Rogers-Ka@users.noreply.github.com>
2020-12-16 20:50:29 +0300

    Update settings.py
    allowed hosts

--- buck/buck/settings.py
+++ buck/buck/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'eiopvd+xv-6w2spgc^ydsnh#w(8=gg@p1f_l)m=*mpsok5#-7$'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1']
+ALLOWED_HOSTS = ['127.0.0.1', 'buckshops.herokuapp.com/']
 
 templ = os.path.join(BASE_DIR, 'Template')
 
